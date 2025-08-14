4ae749904c2b4899afa273914c88178e626ec435
brant <brant@email.com>
2025-08-14 20:00:56 -0300

    Aula 2

---
+++ receitas/urls.py
@@ -0,0 +1,6 @@
+# receitas/urls.py
+from django.urls import path
+from . import views
+urlpatterns = [
+path('', views.home, name='home'),
+]
---
+++ receitas/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render 
+
+def home(request):
+ return render(request, 'receitas/home.html')
+# Create your views here.
