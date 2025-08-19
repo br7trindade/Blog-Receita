2f479e025bbeff2252b4049be99b51947c9c8ef8
brant <brant@email.com>
2025-08-19 20:40:48 -0300

    fotos

--- receitas/views.py
+++ receitas/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render 
+from django.http import Http404
 
 def home(request):
+ 
  return render(request, 'receitas/home.html')
-# Create your views here.
+
+def receita_detail(request, id):
+ 
+    context = {
+       'receita_id': id,
+       'receita_title': f'Receita Detalhada {id}',
+       'receita_description': f'Esta e a descrição detalhada da receita com ID {id}' 
+    }
+ 
+
